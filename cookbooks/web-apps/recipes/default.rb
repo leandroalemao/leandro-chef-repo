ed1d153543edfa0b4acf56be3ccb73c85472cba9
Leandro <leandro.costantini@gmail.com>
2014-11-18 11:17:09 +0000

    basic chef + knife stack app

--- cookbooks/web-apps/recipes/default.rb
+++ cookbooks/web-apps/recipes/default.rb
@@ -6,3 +6,19 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+user "web" do
+  action :create
+end
+
+group "web" do
+  members ["web", "ubuntu"]
+  action :create
+end
+
+directory "/var/www" do
+  owner 'web'
+  group 'web'
+  mode '0644'
+  action :create
+end
